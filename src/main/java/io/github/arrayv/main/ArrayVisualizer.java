77eb1e3a8ebae3edca8a2f8c086e0511bd884318
aphitorite <66573194+aphitorite@users.noreply.github.com>
2026-08-17 22:28:58 -0600

    import iterator

--- src/main/java/io/github/arrayv/main/ArrayVisualizer.java
+++ src/main/java/io/github/arrayv/main/ArrayVisualizer.java
@@ -40,6 +40,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.IdentityHashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
