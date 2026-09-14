b41dd47b3f5af99b71cdc387e057b43bbd7110b0
aphitorite <66573194+aphitorite@users.noreply.github.com>
2026-09-14 16:11:46 -0600

    better progress

--- src/main/java/io/github/arrayv/panes/RenderProgressDialog.java
+++ src/main/java/io/github/arrayv/panes/RenderProgressDialog.java
@@ -7,7 +7,6 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JProgressBar;
 import javax.swing.Timer;
 
 import io.github.arrayv.main.ArrayVisualizer;
@@ -23,18 +22,17 @@ public final class RenderProgressDialog extends JDialog {
 
     public RenderProgressDialog(ArrayVisualizer arrayVisualizer, OfflineVideoRenderer renderer) {
         super(arrayVisualizer.getMainWindow(), "Rendering Video", false);
+        
+        JLabel label1 = new JLabel("Time elapsed: 00:00:00");
+        JLabel label2 = new JLabel("Length: ________ frames (00:00:00)");
 
-        JProgressBar bar = new JProgressBar();
-        bar.setIndeterminate(true);
-        JLabel label = new JLabel("Rendering...");
-
-        JButton cancel = new JButton("Cancel Render");
+        JButton cancel = new JButton("Abort");
         cancel.addActionListener(e -> arrayVisualizer.setCanceled(true));
 
         JPanel content = new JPanel(new BorderLayout(8, 8));
         content.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
-        content.add(label, BorderLayout.NORTH);
-        content.add(bar, BorderLayout.CENTER);
+        content.add(label1, BorderLayout.NORTH);
+        content.add(label2, BorderLayout.CENTER);
         content.add(cancel, BorderLayout.SOUTH);
 
         setContentPane(content);
@@ -45,8 +43,10 @@ public final class RenderProgressDialog extends JDialog {
 
         this.timer = new Timer(250, e -> {
             long frames = renderer.getFramesWritten();
+            long length = frames / renderer.getFps();
             long seconds = renderer.getElapsedMillis() / 1000L;
-            label.setText("Rendering... " + frames + " frames (" + seconds + "s)");
+            label1.setText("Time elapsed: " + String.format("%02d:%02d:%02d", seconds / 3600, (seconds % 3600) / 60, seconds % 60));
+            label2.setText("Length: " + frames + " frames (" + String.format("%02d:%02d:%02d", length / 3600, (length % 3600) / 60, length % 60) + ")");
         });
         this.timer.start();
         setVisible(true);
